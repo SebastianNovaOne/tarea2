de6b7bcb0366ca6927e3b056d263cf7908e374a5
SebastianNovaOne <sebaxnovax@gmail.com>
2025-06-02 18:39:01 -0400

    Se crea clase Empleado que implements Invitable. Se añaden atributos asociados.

---
+++ src/main/java/Empleado.java
@@ -0,0 +1,8 @@
+public class Empleado implements Invitable {
+    private String id;
+    private String apellidos;
+    private String nombre;
+    private String correo;
+    private Departamento departamento;
+}
+
