f1ada80d7bd6b453f71bc9398476a3f14feeb4f1
SebastianNovaOne <sebaxnovax@gmail.com>
2025-06-02 18:40:55 -0400

    Se añade constructor.

--- src/main/java/Empleado.java
+++ src/main/java/Empleado.java
@@ -4,5 +4,13 @@ public class Empleado implements Invitable {
     private String nombre;
     private String correo;
     private Departamento departamento;
+
+    public Empleado(String id, String apellidos, String nombre, String correo, Departamento departamento) {
+        this.id = id;
+        this.apellidos = apellidos;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.departamento = departamento;
+    }
 }
 
