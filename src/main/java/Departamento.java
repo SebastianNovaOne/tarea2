fb9c5b2dccf71948a4af5e35458a6c15b62fd784
SebastianNovaOne <sebaxnovax@gmail.com>
2025-06-02 18:23:42 -0400

    Se añaden getters and setters de los atributos y se añade el metodo toString

--- src/main/java/Departamento.java
+++ src/main/java/Departamento.java
@@ -16,4 +16,23 @@ public class Departamento {
     public int obtenerCantidadEmpleados() {
         return empleados.size();
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(List<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+
+    public String toString() {
+        return nombre + " (" + empleados.size() + " empleados)";
+    }
 }
