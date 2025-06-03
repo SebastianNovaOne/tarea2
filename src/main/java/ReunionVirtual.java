d2862884b8916df2eb6374b2963dbdff8a27b312
SebastianNovaOne <sebaxnovax@gmail.com>
2025-06-03 01:36:07 -0400

    Se añade getter and setter

--- src/main/java/ReunionVirtual.java
+++ src/main/java/ReunionVirtual.java
@@ -9,4 +9,13 @@ public class ReunionVirtual extends Reunion {
         super(fecha, horaPrevista, duracionPrevista, organizador, tipo);
         this.enlace = enlace;
     }
+
+    public String getEnlace() {
+        return enlace;
+    }
+
+    public void setEnlace(String enlace) {
+        this.enlace = enlace;
+    }
+
 }
