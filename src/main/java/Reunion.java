1adf7542cdc71057d8d594a78cd9f98fd5324480
SebastianNovaOne <sebaxnovax@gmail.com>
2025-06-03 00:09:45 -0400

    Se crea clase abstracta Reunion, y se añaden los atributos y el constructor correspondientes acorde al UML del enunciado.

---
+++ src/main/java/Reunion.java
@@ -0,0 +1,22 @@
+import java.time.*;
+import java.util.*;
+
+public abstract class Reunion {
+    private Date fecha;
+    private Instant horaPrevista;
+    private Duration duracionPrevista;
+    private Instant horaInicio;
+    private Instant horaFin;
+    private Empleado organizador;
+    private TipoReunion tipo;
+    List<Invitacion> listaInvitados = new ArrayList<>();
+    List<Nota> notas = new ArrayList<>();
+
+    public Reunion(Date fecha, Instant horaPrevista, Duration duracionPrevista, Empleado organizador, TipoReunion tipo) {
+        this.fecha = fecha;
+        this.horaPrevista = horaPrevista;
+        this.duracionPrevista = duracionPrevista;
+        this.organizador = organizador;
+        this.tipo = tipo;
+    }
+}
