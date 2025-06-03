5ac2c3c6b69bb00efeeebfc8c411710fe8e586f8
SebastianNovaOne <sebaxnovax@gmail.com>
2025-06-02 22:48:53 -0400

    Se añaden metodos marcarPresente y marcarRetraso, ademas de todos los getters and setters mas el metodo toString

--- src/main/java/Invitacion.java
+++ src/main/java/Invitacion.java
@@ -11,4 +11,50 @@ public class Invitacion {
         this.hora = hora;
         this.estado = EstadoAsistencia.AUSENTE;
     }
+
+    public void marcarPresente() {
+        this.estado = EstadoAsistencia.PRESENTE;
+    }
+
+    public void marcarRetraso(Instant llegada) {
+        this.estado = EstadoAsistencia.RETRASADO;
+        this.retraso = new Retraso(llegada);
+    }
+
+    public Invitable getInvitado() {
+        return invitado;
+    }
+
+    public void setInvitado(Invitable invitado) {
+        this.invitado = invitado;
+    }
+
+    public EstadoAsistencia getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoAsistencia estado) {
+        this.estado = estado;
+    }
+
+    public Retraso getRetraso() {
+        return retraso;
+    }
+
+    public void setRetraso(Retraso retraso) {
+        this.retraso = retraso;
+    }
+
+    public Instant getHora() {
+        return hora;
+    }
+
+    public void setHora(Instant hora) {
+        this.hora = hora;
+    }
+
+    public String toString() {
+        return invitado.toString() + " - " + estado +
+                (retraso != null ? " (llego a: " + retraso.getHora() + ")" : "");
+    }
 }
