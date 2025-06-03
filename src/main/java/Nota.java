9956128078072569136a8c581d65adaa57351d11
SebastianNovaOne <sebaxnovax@gmail.com>
2025-06-02 20:18:01 -0400

    Se añaden getters y setters y metodo toString

--- src/main/java/Nota.java
+++ src/main/java/Nota.java
@@ -8,4 +8,24 @@ public class Nota {
         this.contenido = contenido;
         this.fechaCreacion = Instant.now();
     }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public Instant getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Instant fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public String toString() {
+        return "[" + fechaCreacion + "]" + contenido;
+    }
 }
